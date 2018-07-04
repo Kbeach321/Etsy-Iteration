7ba6bc8a83796da3bf9e49ce0530f19de02ac5d1
Kbeach321 <iamkevinbeach@gmail.com>
2018-07-04 10:53:39 -0400

    inital setup

---
+++ main.js
@@ -0,0 +1,16 @@
+// Average Price of All Items //
+
+
+// Array of items that cost between $14 - $18 USD //
+
+
+// Find item with GBP Currency and print NAME & PRICE //
+
+
+// Show items made of wood //
+
+
+// Show items made of 8+ materials //
+
+
+// Calculate how many items are made by their sellers //
